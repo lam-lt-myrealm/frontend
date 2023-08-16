a3626390e7732250bcc28ff294ab11d3f3703d7a
Le Tung Lam <letunglam1809.hust@gmail.com>
2023-08-16 10:57:29 +0700

    SPA React with Keycloak SSO Viettel Cloud

--- src/Keycloak.js
+++ src/Keycloak.js
@@ -1,7 +1,7 @@
 import Keycloak from 'keycloak-js'
 
 const keycloak = new Keycloak({
-    url: 'http://localhost:8080',
+    url: 'https://sso.vcloud.viettel.vn',
     realm: 'my-realm',
     clientId: 'my-client'
 });
